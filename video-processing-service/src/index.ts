b61ee4cfdfb6c43850702babb5cd95fb654e163c
Wajahat <aliwajahat44@yahoo.com>
2023-07-27 18:40:22 +0500

    Created video processing to process local videos and contanirized it using docker

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,13 +1,57 @@
 import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
-const PORT = 2000;
+//Tell express to use json as body parser
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+
+app.post('/process-video', (req, res) => {
+    //Getting path of input video
+    const inputFilePath = req.body.inputFilePath;
+    //Getting path of output video
+    const outputFilePath = req.body.outputFilePath;
+
+    //checking if input and output file paths are provided
+    if (!inputFilePath || !outputFilePath) {
+        let missingFiles = [];
+        if (!inputFilePath){
+            missingFiles.push( "Input File Path");
+        }
+        if (!outputFilePath){
+            missingFiles.push("Output File Path");
+        }
+
+        let missingFilesStr = missingFiles.join(" and ");
+        res.status(400).send({
+            error: `Bad Request`,
+            message: `Missing ${missingFilesStr}`
+        });
+    }
+
+
+    //Creating ffmpeg command
+    ffmpeg(inputFilePath)
+        .outputOptions("-vf","scale = -1:360p") //setting output video resolution to 360p
+        .on('end', () => {      //when processing is finished
+            console.log('Processing finished !');
+            res.status(200).send({
+                message: `Processing finished !`
+            });
+        })
+        .on('error', (err) => { //when processing is finished with error
+            console.log(`Error ${err.message}`);
+            res.status(500).send({
+                error: `Internal Server Error`,
+                message: `Error: ${err.message}`
+            });
+        })
+        .save(outputFilePath); //saving output video to output file path
+
     }
 );
 
+const PORT = process.env.port || 3000; //Take the port from environment variable or use 2000 as default (important for deployment)
 
 app.listen(PORT, () => {
     console.log(`⚡️[Video Processing Service]: listening at http://localhost:${PORT}`);
